3c56c25b3dfc6cf11c6608993e2b269ff02c0ddd
Gary Gregory <garydgregory@gmail.com>
2023-10-31 06:22:24 -0400

    Normalize spelling to US English

--- src/main/java/org/apache/commons/jexl3/introspection/JexlPermissions.java
+++ src/main/java/org/apache/commons/jexl3/introspection/JexlPermissions.java
@@ -58,7 +58,7 @@ import java.util.stream.Collectors;
  *     {@link #UNRESTRICTED} as parameter before creating a JEXL engine instance.
  * </p>
  * <p>
- *     For the same reason, using JEXL through scripting, it is possible to revert the underlying JEXL behaviour to
+ *     For the same reason, using JEXL through scripting, it is possible to revert the underlying JEXL behavior to
  *     JEXL 3.2 default by calling {@link org.apache.commons.jexl3.scripting.JexlScriptEngine#setPermissions(JexlPermissions)}
  *     with {@link #UNRESTRICTED} as parameter.
  * </p>
