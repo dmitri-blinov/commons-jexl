ae1a0f985d7e371d5a487d7107ffea82ba2feda2
Dmitri Blinov <dmitri_blinov@mail.ru>
2024-05-03 12:21:34 +0300

    Add Comparable implementation to Source to allow HashMap optimizations in jdk 8

--- src/main/java/org/apache/commons/jexl3/internal/Source.java
+++ src/main/java/org/apache/commons/jexl3/internal/Source.java
@@ -25,7 +25,7 @@ import java.util.Objects;
  * <p>This is meant for caching scripts using their 'source' as key but still distinguishing
  * scripts with different features and prevent false sharing.
  */
-public final class Source {
+public final class Source implements Comparable<Source> {
     /** The hash code, pre-computed for fast op. */
     private final int hashCode;
     /** The set of features. */
@@ -52,6 +52,11 @@ public final class Source {
      */
     int length() {
         return str.length();
+    }
+
+    @Override
+    public int compareTo(Source s) {
+        return str.compareTo(s.str);
     }
 
     @Override
